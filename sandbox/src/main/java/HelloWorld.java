417f52fed26f07a2db5265550e110709d3092c02
YummyMirror <anatolianiukevich@gmail.com>
2017-04-06 22:50:42 +0300

    Have created project 'sandbox'

---
+++ sandbox/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by anatoli.anukevich on 4/6/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello world. This is the first one file for my new repository.");
+    }
+}
