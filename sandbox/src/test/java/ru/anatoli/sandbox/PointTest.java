26c0d55c70a12c78f702e8573872deb0ca57f5a1
YummyMirror <anatolianiukevich@gmail.com>
2017-04-14 09:06:56 +0300

    Have added home Task 2.1 with test for correct calculation between 2 points

---
+++ sandbox/src/test/java/ru/anatoli/sandbox/PointTest.java
@@ -0,0 +1,39 @@
+package ru.anatoli.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by anatoli.anukevich on 4/13/2017.
+ */
+public class PointTest {
+    //Checking correct distance calculation 1
+    @Test
+    public void distanceTest() {
+        Point p = new Point();
+        Point p1 = new Point(5, 5);
+        Point p2 = new Point(10, 10);
+
+        Assert.assertEquals(p.distance(p1, p2), 7.0710678118654755);
+    }
+
+    //Checking correct distance calculation 2
+    @Test
+    public void distanceTest2() {
+        Point p = new Point();
+        Point p1 = new Point(10, 10);
+        Point p2 = new Point(20, 20);
+
+        Assert.assertEquals(p.distance(p1, p2), 14.142135623730951);
+    }
+
+    //Checking correct distance calculation 3
+    @Test
+    public void distanceTest3() {
+        Point p = new Point();
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(50, 50);
+
+        Assert.assertEquals(p.distance(p1, p2), 69.29646455628166);
+    }
+}
