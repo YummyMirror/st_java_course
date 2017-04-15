e441a7f0da000a02ce686cfed7de404206013a5e
YummyMirror <anatolianiukevich@gmail.com>
2017-04-15 11:45:14 +0300

    Have renamed test signature in 'Test' annotation

--- addressbook/src/test/java/ru/anatoli/addressbook/ContactDeletionTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/ContactDeletionTests.java
@@ -38,6 +38,7 @@ public class ContactDeletionTests {
     }
 
     @Test
+    public void testContactDeletion() {
         ContactData contactData = new ContactData("1", "2", "3", "4", "5", "6", "7");
 
         goToNewContactPage();
