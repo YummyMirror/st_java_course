288acfd8df87040b6d1f1024f97b90a2931cd034
YummyMirror <anatolianiukevich@gmail.com>
2017-06-11 19:45:53 +0300

    Have added Selenium Server to run TCs on it

--- addressbook/src/test/java/ru/anatoli/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/appmanager/ApplicationManager.java
@@ -1,17 +1,21 @@
 package ru.anatoli.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import ru.anatoli.addressbook.models.UserData;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -47,13 +51,20 @@ public class ApplicationManager {
 
         dbHelper = new DbHelper();
         //Choosing browser
-        if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        }else if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        }else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.FIREFOX)) {
+                wd = new FirefoxDriver();
+            }else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            }else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+            }
+        } else {
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+            desiredCapabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), desiredCapabilities);
         }
+
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
